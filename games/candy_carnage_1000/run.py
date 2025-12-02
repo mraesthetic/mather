22627e54f4e13b76cc7a62b01440612ca460a4db
mraesthetic <68799577+mraesthetic@users.noreply.github.com>
2025-12-02 15:00:45 +1100

    lol

--- games/candy_carnage_1000/run.py
+++ games/candy_carnage_1000/run.py
@@ -11,9 +11,9 @@ from src.write_data.write_configs import generate_configs
 
 if __name__ == "__main__":
 
-    num_threads = 10
-    rust_threads = 20
-    batching_size = 10000
+    num_threads = 32
+    rust_threads = 64
+    batching_size = 5000
     compression = True
     profiling = False
 
